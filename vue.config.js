f841e0f71aa449588d4f257fe0aa734053616672
杜帅(dushuai) <dushuai@qdingnet.cn>
2020-09-21 10:59:25 +0800

    add color config

--- vue.config.js
+++ vue.config.js
@@ -3,15 +3,22 @@ module.exports = {
     loaderOptions: {
       less: {
         modifyVars: {
+          // main color
           primary: "#4894ff",
+          // success color, handle success state, so do error text
           success: "#1dd55d",
           error: "#ff416c",
           text: "#7a88b5",
+          // title color, use for emphase
           title: "#253156",
           background: "#eff2f5",
-          radius: ".8em",
-          gutter: ".6em",
+          // card button etc. border radius
+          radius: ".4em",
+          // the gutter bettween some elements
+          gutter: ".8em",
+          // content background
           content: "#ffffff",
+          // box shadow
           shadow: "0 .5em 1em 0",
         },
       },
